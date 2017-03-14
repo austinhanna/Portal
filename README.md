81a57c980aee783931cf0ecbc055849037d85d06
Austin Hanna <austinbhanna@gmail.com>
2017-03-14 22:39:25 +1100

    Merges and readme updates

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Portal
 My Magic Mirror Prototype!
 
-#A Magic Mirror written in Python with Kivy
+#A Magic Mirror written in Python with PyQt5
+You can get PyQt5 at https://sourceforge.net/projects/pyqt/
+It is available for me to use under a GPL license.
--- portal/pyQT.py
+++ portal/pyQT.py
@@ -1,10 +1,15 @@
 import sys
 from PyQt5 import QtWidgets
+from PyQt5 import QtGui
 
 def window():
     app = QtWidgets.QApplication(sys.argv)
     w = QtWidgets.QWidget()
     la = QtWidgets.QLabel(w)
+    font = QtGui.QFont()
+    font.setFamily("Helvetica")
+    font.setBold(True)
+    la.setFont(font)
     la.setText("Hello World!")
     w.setWindowTitle("Test!")
     w.setGeometry(100,100,300,200)
