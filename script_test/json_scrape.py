d22d4144bfb19f0d782f307ead81f91b63ff03c3
Austin Hanna <austinbhanna@gmail.com>
2017-03-14 23:33:36 +1100

    Begin JSON scraping!

---
+++ script_test/json_scrape.py
@@ -0,0 +1,3 @@
+import requests
+city=raw_input("Enter the name of the City to know it's weather... ")
+url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=40460bd21017b5384bdfcbf78da21cc8/"
