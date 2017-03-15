4816253e54e2a6f1c3ae9317e3ad8c82a8ae46e6
Austin Hanna <austinbhanna@gmail.com>
2017-03-15 19:56:41 +1100

    Twitter!
    and Donald Trump

--- portal/twitter/streaming.py
+++ portal/twitter/streaming.py
@@ -1,27 +1,32 @@
-import time
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+import time
+import json
 
 ckey = 'WEzuoWntc2NqKVg0FSnhlmDNR'
-csec = 'Rq4dcMN9gGo1JWaglibGUDqDLLPFpPkbQOhYlMSaJRfPcIjnuF'
+csecret = 'Rq4dcMN9gGo1JWaglibGUDqDLLPFpPkbQOhYlMSaJRfPcIjnuF'
 atoken = '1905281930-DWTNXwF3F50Zu5zfbKEvKssuu9ejkbawHN06Tga'
-asec = '2GaOj1TTMGY2ZYEXkOOolZRipIyu9cdB41X5Bkj2W4UkA'
+asecret = '2GaOj1TTMGY2ZYEXkOOolZRipIyu9cdB41X5Bkj2W4UkA'
 
 class listener(StreamListener):
-    def on_data(self, data):
-        try:
-            #print(data)
-            tweet = data.split(',"text":"')[1].split('","source')[0]
-            print(tweet)
-            return True
-        except BaseException:
-            print(str(e))
-            time.sleep(5)
-    def on_error(self, status):
-        print(status)
 
-auth = OAuthHandler(ckey, csec)
-auth.set_access_token(atoken, asec)
+    def on_data(self, data):
+        all_data = json.loads(data)
+
+        tweet = all_data["text"].encode('UTF-8')
+
+        username = all_data["user"]["screen_name"].encode('UTF-8')
+
+        print(username,tweet)
+
+        return True
+
+    def on_error(self, status):
+        print (status)
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
 twitterStream = Stream(auth, listener())
-twitterStream.filter(track=['Car'])
+twitterStream.filter(track=["trump"])
