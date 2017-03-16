80dae245575a3af5af3228dd5ee6a767d12a3dbc
Austin Hanna <austinbhanna@gmail.com>
2017-03-17 09:20:50 +1100

    Good Goyim

--- script_test/pyQT.py
+++ script_test/pyQT.py
@@ -7,13 +7,22 @@ from PyQt5.QtGui import QPainter, QColor, QPen
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 from PyQt5 import QtCore
+from PyQt5.QtCore import QTime, QTimer
+from PyQt5.QtWidgets import QApplication, QLCDNumber
 
 def window():
     app = QtWidgets.QApplication(sys.argv)
 
+    while True != False:
+        time = QTime.currentTime()
+        text = time.toString('hh:mm')
+        if (time.second() % 2) == 0:
+            text = text[:2] + ' ' + text[3:]
+
     # Create our elements #
     w = QtWidgets.QWidget() # The window itself
     la1 = QtWidgets.QLabel(w) # Label 1
+    la2 = QtWidgets.QLabel(w) # Label 1
 
     font = QtGui.QFont() # Make font element
     fontp = QtGui.QPalette()
@@ -22,13 +31,17 @@ def window():
     font.setPointSize(72)
     font.setBold(False) # Bold?
     la1.setFont(font)
+    la2.setFont(font)
     #font.setColor(Qt.white)
 
     fontp.setColor(QtGui.QPalette.Foreground,QtCore.Qt.white)
     la1.setPalette(fontp)
+    la2.setPalette(fontp)
 
-    la1.setGeometry(560,330,1100,500)
+    #la1.setGeometry(0,330,1100,500)
+    la2.setGeometry(560,330,1100,500)
     la1.setText("Welcome") # Set label text
+    la2.setText(text) # Set label text
 
     w.setWindowTitle("Test!") # Set window title text
 
