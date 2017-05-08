d9dacc70853c9eaa732da705e745f884652449d5
Austin Hanna <austinbhanna@gmail.com>
2017-05-08 14:26:48 +1000

    yeyeye

--- portal/twitter/config.py
+++ portal/twitter/config.py
@@ -9,7 +9,7 @@ config['Auth'] = {}
 ## Edit below this line! ##
 
 # Which keyword would you like to show tweets from?
-config['General']['Keyword'] = 'Python' # Show tweets by keyword
+config['General']['Keyword'] = 'IEM' # Show tweets by keyword
 #config['General']['Trend'] = '#Python' -- To be added soon, Show tweets by trend
 #config['General']['Person'] = '@Python' -- To be added soon, Show tweets by person
 #blacklist = ['enter words that tweets may contain you don't want to see...'] # This will filter any tweet that has any keyword in it. Opposite of the Keyword setting above.
