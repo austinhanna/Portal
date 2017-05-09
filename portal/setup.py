760231c896ddb38f7de77587594cc174224e3706
Austin Hanna <austinbhanna@gmail.com>
2017-05-09 16:44:50 +1000

    Gave config.ini some friends

--- portal/setup.py
+++ portal/setup.py
@@ -8,7 +8,11 @@ virgin = True
 
 def configurator():
     if virgin == True:
-        config['User Data'] = {}
+        global time_en
+        global weath_en
+        global twit_en
+        global news_en
+        global u_name
 
         ### Begin Info ###
         print('Running Configurator...')
@@ -21,7 +25,7 @@ def configurator():
         print("This program will help you setup everything you need to get your mirror customised to your needs.")
         time.sleep(1)
         print("First, we will ask some simple questions.")
-        time.sleep(1)
+        time.sleep(3)
         print()
         ###
 
@@ -32,18 +36,39 @@ def configurator():
         print('Hi, '+u_name+'!')
         print()
 
+        print("Module Setup")
+        time.sleep(3)
+        print("Please answer each with a True/False")
+        time.sleep(2)
+        print()
+        time_en = input('Would you like to enable the Time module?(True/False) ')
+        print()
+        weath_en = input('Would you like to enable the Weather module?(True/False) ')
+        print()
+        twit_en = input('Would you like to enable the Twitter module?(True/False) ')
+        print()
+        news_en = input('Would you like to enable the News module?(True/False) ')
+        print()
+        print('Writing to file...')
+        time.sleep(1)
+        print()
+        print("DONE!")
+        print()
+        print('Configurator done.')
 
-        
-        ### Append configs ###
-        config['User Data']['Name'] = u_name
-        with open('userdata.ini', 'w') as configfile:
-            config.write(configfile)
+        ### Send 'em back! ###
+        return time_en
+        return weath_en
+        return twit_en
+        return news_en
+        return u_name
 configurator()
 
 def cunfig():
     # If you are making a module you can add it's config below. #
     # config['NAME'] = {}
     config['General'] = {}
+    config['User Data'] = {}
     config['Placement'] = {}
     config['Modules'] = {}
     config['Time Module'] = {}
@@ -56,7 +81,7 @@ def cunfig():
     ## Edit below this line! ##
     # General Settings #
 
-    config['General']['FirstTime'] = 'True'
+    config['General']['FirstTime'] = 'ye'
 
     # Placement Settings #
 
@@ -69,40 +94,49 @@ def cunfig():
 
     # Modules #
 
-    config['Modules']['local_time'] = 'Enabled'
-    config['Modules']['other_time'] = 'Disabled'
-    config['Modules']['local_weather'] = 'Enabled'
-    config['Modules']['other_weather'] = 'Disabled'
-    config['Modules']['twitter_feed'] = 'Enabled'
-    config['Modules']['news_ticker'] = 'Enabled'
+    config['Modules']['time'] = time_en
+    config['Modules']['weather'] = weath_en
+    config['Modules']['twitter_feed'] = twit_en
+    config['Modules']['news_ticker'] = news_en
 
     # Time Module #
 
-    config['Time Module']['local_time'] = 'Enabled' # Should stay enabled unless you want the current time on mars for some reason...
-    config['Time Module']['get_from_system'] = 'Enabled' # If enabled then the module will pull the current time from the PC rather than the internet. This can be reliable if there is no internet connection.
-    #config['Time Module']['other_time'] = ''
-
+    if time_en == True:
+        config['Time Module']['local_time'] = 'Enabled' # Should stay enabled unless you want the current time on mars for some reason...
+        config['Time Module']['get_from_system'] = 'Enabled' # If enabled then the module will pull the current time from the PC rather than the internet. This can be reliable if there is no internet connection.
+        #config['Time Module']['other_time'] = ''
+    else:
+        config['Time Module']['local_time'] = 'Disabled'
+        #config['Time Module']['get_from_system'] = 'Disabled' MAKE THIS SEPERATE...
     # Weather Module #
 
-    config['Weather Module']['local_weather'] = 'Enabled'
-    config['Weather Module']['country_name'] = 'nil'
-    config['Weather Module']['city_name'] = 'nil'
+    if weath_en == True:
+        config['Weather Module']['local_weather'] = 'Enabled'
+        #config['Weather Module']['country_name'] = 'nil'
+        #config['Weather Module']['city_name'] = 'nil'
+    else:
+        config['Weather Module']['local_weather'] = 'Disabled'
 
     # Twitter Module #
-
-    config['Twitter Module']['twitter_live_feed'] = 'Enabled'
-    config['Twitter Module']['live_feed_keyword'] = 'Enabled'
-    config['Twitter Module']['live_feed_word'] = 'nil'
-    config['Twitter Module']['live_feed_trending'] = 'Disabled'
-    config['Twitter Module']['live_feed_people'] = 'Disabled'
-    config['Twitter Module']['live_feed_person'] = '@nil'
+    if twit_en == True:
+        config['Twitter Module']['twitter_live_feed'] = 'Enabled'
+        config['Twitter Module']['live_feed_keyword'] = 'Enabled'
+        config['Twitter Module']['live_feed_word'] = 'nil'
+        config['Twitter Module']['live_feed_trending'] = 'Disabled'
+        config['Twitter Module']['live_feed_people'] = 'Disabled'
+        config['Twitter Module']['live_feed_person'] = '@nil'
+    else:
+        print('do something here') # live feed disable
 
     # News Module #
+    if news_en == True:
+        config['News Module']['news_ticker'] = 'Enabled'
+        config['News Module'][''] = ''
+    else:
+        config['News Module']['news_ticker'] = 'Enabled'
 
-    config['News Module']['news_ticker'] = 'Enabled'
-    config['News Module'][''] = ''
+    config['User Data']['Name'] = u_name
 
-
-    # Okay don't touch this either. #
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
+cunfig()
