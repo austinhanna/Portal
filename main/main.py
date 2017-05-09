f72c2c622075517a8c086fa945b71172a1254039
Austin Hanna <austinbhanna@gmail.com>
2017-05-10 08:37:15 +1000

    Jam it in... I mean, Fit it nicely.

---
+++ main/main.py
@@ -0,0 +1,10 @@
+import sys
+import time
+import datetime
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
+from PyQt5.QtGui import QPainter, QColor, QPen
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
+from PyQt5 import QtCore
