9e56b7a2da9989edb96e65ae5a13af1d8425fafd
Austin Hanna <austinbhanna@gmail.com>
2017-05-10 13:30:38 +1000

    Tweets and Rheadlines display properly. (Almost) all done for the day

--- main/main.py
+++ main/main.py
@@ -39,20 +39,43 @@ def main():
         app = QtWidgets.QApplication(sys.argv)
 
         # Create our elements #
-        w = QtWidgets.QWidget() # The window itself
+        w = QtWidgets.QWidget() # The window
+
         la1 = QtWidgets.QLabel(w) # Label 1
+        tweet = QtWidgets.QLabel(w) # Label "Tweet"
+        headline = QtWidgets.QLabel(w) # Label "Reddit"
+        la2 = QtWidgets.QLabel(w) # Label "Reddit"
 
         font = QtGui.QFont() # Make font element
         fontp = QtGui.QPalette()
-
         font.setFamily("Tahoma") # Set Font
-        font.setPointSize(12)
+        font.setPointSize(32)
         font.setBold(False) # Bold?
+
+        lt_font = QtGui.QFont() # Make font element
+        lt_font.setFamily("Tahoma") # Set Font
+        lt_font.setPointSize(16)
+        lt_font.setBold(False) # Bold?
+
         la1.setFont(font)
+        la2.setFont(font)
+        tweet.setFont(lt_font)
+        headline.setFont(lt_font)
+
         fontp.setColor(QtGui.QPalette.Foreground,QtCore.Qt.white) # Label Color
         la1.setPalette(fontp) # Set label to palette
-        la1.setGeometry(0,0,2500,500)
-        la1.setText("Hello.")
+        la2.setPalette(fontp) # Set label to palette
+        tweet.setPalette(fontp) # Set label to palette
+        headline.setPalette(fontp) # Set label to palette
+
+        la1.move(10,10)
+        la2.move(10,100)
+        tweet.move(860,1000)
+        headline.move(10,500)
+        tweet.setGeometry(0,750,1920,500)
+        headline.setGeometry(0,0,1920,500)
+        la1.setText("Hello")
+        la2.setText("there.")
         w.setWindowTitle("Main") # Set window title text
 
         # Color #
@@ -63,54 +86,21 @@ def main():
         w.show()
         #os.system("cd Twitter")
         #os.system("python streaming.py 1")
-        #w.showFullScreen() # Make window fullscreen
-        sys.exit(app.exec_())
-    parentmodule()
+        w.showFullScreen() # Make window fullscreen
 
-    def twittermodule():
-        app = QtWidgets.QApplication(sys.argv)
-
-        # Create our elements #
-        w = QtWidgets.QWidget() # The window itself
-        la1 = QtWidgets.QLabel(w) # Label 1
-
-        font = QtGui.QFont() # Make font element
-        fontp = QtGui.QPalette()
-
-        font.setFamily("Tahoma") # Set Font
-        font.setPointSize(12)
-        font.setBold(False) # Bold?
-        la1.setFont(font)
-        fontp.setColor(QtGui.QPalette.Foreground,QtCore.Qt.white) # Label Color
-
-
-        la1.setPalette(fontp) # Set label to palette
-
-        la1.setGeometry(0,0,2500,500) # Label size and positioning
         def update_label():
-            with open('twitter/store/tweet.txt', 'r',encoding="utf8") as content_file:
-                content = content_file.read()
+            with open('bin/headlines.txt', 'r',encoding="utf-8") as headline_file:
+                reddit_content = headline_file.read()
+            with open('twitter/store/tweet.txt', 'r',encoding="utf8") as tweet_file:
+                tweet_content = tweet_file.read()
             current_time = str(datetime.datetime.now().time())
-            la1.setText(content) # Split this! Add @ and says.
-
+            tweet.setText(tweet_content) # Split this! Add @ and says.
+            headline.setText(reddit_content)
         timer = QtCore.QTimer()
         timer.timeout.connect(update_label)
-        timer.start(1000)  # Check for new tweet every second
-
-         # Set label text
-        w.setWindowTitle("Main") # Set window title text
-
-        # Color #
-        w.setAutoFillBackground(True) # Fill
-        p = w.palette() # Make P palette for window and color options
-        p.setColor(w.backgroundRole(), Qt.black) # Set window color
-        w.setPalette(p) #
-        w.show()
-        #os.system("cd Twitter")
-        #os.system("python streaming.py 1")
-        #w.showFullScreen() # Make window fullscreen
+        timer.start(500)  # Check for new tweet/headline every second
         sys.exit(app.exec_())
-    #twittermodule()
+    parentmodule()
 
 
 # Launch Control #
