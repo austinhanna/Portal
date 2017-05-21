c2c3280719a489df3ea6f158ac037a8765af6f8b
Austin Hanna <austinbhanna@gmail.com>
2017-05-21 19:10:27 +1000

    No install script. Follow the damn readme.

--- install.py
+++
@@ -1,7 +0,0 @@
-print("This script will install all dependencies needed to run the main program. ONLY RUN THIS ONCE.")
-# Detect pre-installed dependencies
-# Status of dependencies
-# Updated?
-# Correct Python version
-# Should be all good. Launch Setup?
-# Once launched, move file / delete.
