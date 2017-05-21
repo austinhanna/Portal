caf9d96a78028a0bf6ac3a817e448cc36555e3ef
Austin Hanna <austinbhanna@gmail.com>
2017-05-21 19:07:28 +1000

    Obsolete Testing Scripts, beginning install script

--- exprimental/test.py
+++
@@ -1,5 +0,0 @@
-varp = True
-import time
-while varp == True:
-    print("Varp")
-    time.sleep(.8)
--- exprimental/weather.py
+++
@@ -1,8 +0,0 @@
-import requests
-city = input("Which City do you want to pull weather from? ")
-url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=40460bd21017b5384bdfcbf78da21cc8"
-data = requests.get(url)
-read = data.json()
-print("City Name: "+read['name'])
-print("Temperature: "+str(read['main']['temp']-273.15))
-print(read['weather'][0]['description'])
